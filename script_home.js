a2ca9805499270cdcefeb9421895f8b7cd6736b5
Carlos-Eduardo-de-Arruda-Chaves <carlos.chaves@sptech.school>
2026-04-25 20:18:46 -0300

    Comentando o código js da página Home

--- script_home.js
+++ script_home.js
@@ -1,3 +1,4 @@
+    // Função para marcar a caixa "publico" na página Home
     function marcar_publico(){
         if(document.getElementById('checkbox_publica').checked == false){
             document.getElementById('checkbox_publica').checked = true;
@@ -7,6 +8,7 @@
         }
     }
 
+    // Função para marcar a caixa "privado" na página Home
     function marcar_privado(){
         if(document.getElementById('checkbox_privado').checked == false){
             document.getElementById('checkbox_privado').checked = true;
@@ -16,18 +18,22 @@
         }
     }
 
+    // Função para simulador financeiro da página Home
     function simular(){
+        // Recuperando valores do html inseridos pelo usuário
         let doses = Number(id_doses.value);
         let doses_perdidas = Number(id_doses_perdidas.value);
         let doses_por_lote = Number(id_doses_por_lote.value);
         let valor_vacina = Number(id_valor_unitario.value);
         
+        // Fazendo as contas para retornar o resultado para o usuário
         let porcentagem_perdida = (doses_perdidas / doses) * 100
         let valor_total = doses * valor_vacina
         let qtd_lotes = doses / doses_por_lote
         let valor_lote = valor_vacina * qtd_lotes
         console.log(porcentagem_perdida);
 
+        // Devolvendo o resultado para o usuário no html
         porcentagem.innerHTML = `${porcentagem_perdida.toFixed(2)}%`
         receita_alcancada.innerHTML = `De ${valor_total.toFixed(2)} R$, foi alcançado ${(valor_total - (doses_perdidas * valor_vacina)).toFixed(2)} R$`
         receita_perdida.innerHTML = `${(doses_perdidas * valor_vacina).toFixed(2)} R$`
