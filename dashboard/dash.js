544f1818c89dadc5fcb28047f8d3e32eeb0c7b36
MatheusJacobD <matheus.duarte@sptech.school>
2026-05-13 17:03:55 -0300

    feat: Atualizações na dash de acordo com pedidos do cliente

--- dashboard/dash.js
+++ dashboard/dash.js
@@ -1,6 +1,38 @@
 let graficoLinha = null;
 let graficoPizza = null;
 
+let valores = [];
+let nome;
+let dentro;
+let fora;
+let tempAtual;
+let status;
+let kpis = [];
+
+fechar();
+alertar();
+
+function fechar() {
+    document.getElementById("alertaLote").style.display = 'none';
+}
+
+function abrir(nome, desc) {
+    document.getElementById("alertaTitulo").textContent = nome;
+    document.getElementById("alertaDesc").textContent = desc;
+
+    document.getElementById("alertaLote").style.display = 'block';
+}
+
+function alertar() {
+    if (valores[valores.length - 1] > 8) {
+        abrir(nome, 'O lote está com a temperatura acima da média');
+    } else if (valores[valores.length - 1] < 2) {
+        abrir(nome, 'O lote está com a temperatura abaixo da média');
+    }
+
+    setTimeout(alertar, 2000);
+}
+
 function atualizarOpcoes() {
     const select = document.getElementById("selectSensor");
     const options = select.options;
@@ -15,48 +47,59 @@ function atualizarOpcoes() {
         const ultimo = valoresTemp[valoresTemp.length - 1];
 
         if (ultimo > 8 || ultimo < 2) {
-            options[i - 1].text = `🔴 Sensor ${i}`;
+            options[i - 1].text = `🔴 ABCD${i}`;
         } else {
-            options[i - 1].text = `Sensor ${i}`;
+            options[i - 1].text = `ABCD${i}`;
+        }
+    }
+}
+
+function pesquisarLote() {
+
+    let pesquisa = document.getElementById("inputPesquisa").value.toLowerCase();
+
+    let select = document.getElementById("selectSensor");
+    let options = select.options;
+
+    for (let i = 0; i < options.length; i++) {
+        let texto = options[i].text.toLowerCase();
+        if (texto.includes(pesquisa)) {
+            select.value = options[i].value;
+            chamar(options[i].value);
+            break;
         }
     }
 }
 
 function chamar(sensor) {
 
-    let nome = `Sensor ${sensor}`;
-    let valores = [];
-    let dentro;
-    let fora;
-    let tempAtual;
-    let status;
-    let kpis = [];
+    nome = `Lote ${sensor}`;
 
     if (sensor == 1) {
         valores = [2, 3, 6, 4, 5];
         dentro = 100;
         fora = 0;
-        tempAtual = "5.1C";
+        tempAtual = "5ºC";
         status = "Temperatura normal";
-        kpis = ["3", "1", "0", "0", "00:02:14", "R$3.840"];
+        kpis = ["3", `${valores[valores.length - 1] > 8 || valores[valores.length - 1] < 2 ? 'Sim' : 'Não'}`, "0", "0", "00:00:00", "R$3.840", "José Carvalho Pinto - SA/BH"];
     }
 
     if (sensor == 2) {
         valores = [6, 7, 2, 1, 3];
         dentro = 80;
         fora = 20;
-        tempAtual = "6.8C";
+        tempAtual = "3ºC";
         status = "Temperatura normal";
-        kpis = ["3", "1", "0", "1", "00:03:41", "R$1.275"];
+        kpis = ["3", `${valores[valores.length - 1] > 8 || valores[valores.length - 1] < 2 ? 'Sim' : 'Não'}`, "0", "1", "00:03:41", "R$1.275", "Rua Haddock Lobo 595 - SP/SP"];
     }
 
     if (sensor == 3) {
         valores = [4, 2, 1, 9, 11];
         dentro = 40;
         fora = 60;
-        tempAtual = "9.2C";
+        tempAtual = "11ºC";
         status = "Temperatura acima da média";
-        kpis = ["3", "1", "1.2", "1", "00:04:32", "R$2.583"];
+        kpis = ["3", `${valores[valores.length - 1] > 8 || valores[valores.length - 1] < 2 ? 'Sim' : 'Não'}`, "1.2", "3", "00:04:32", "R$2.583", "Rua Maneira Legal Divertida e Feliz - 67"];
     }
 
     atualizarOpcoes();
@@ -74,12 +117,16 @@ function chamar(sensor) {
         statusEl.style.color = "black";
     }
 
-    document.getElementById("valor1").textContent = kpis[0];
     document.getElementById("valor2").textContent = kpis[1];
     document.getElementById("valor3").textContent = kpis[2];
     document.getElementById("valor4").textContent = kpis[3];
     document.getElementById("valor5").textContent = kpis[4];
     document.getElementById("valor6").textContent = kpis[5];
+    document.getElementById("cardLocalizacaoValue").textContent = kpis[6];
+    document.getElementById("cardRisco").style.background = `${valores[valores.length - 1] > 8 || valores[valores.length - 1] < 2 ? '#ffc0c0' : '#a5b8ff'}`;
+    document.getElementById("cardMedio").style.background = `${Number(kpis[2]) != 0 ? '#ffc0c0' : '#a5b8ff'}`;
+    document.getElementById("cardOcorrencia").style.background = `${Number(kpis[3]) != 0 ? '#ffc0c0' : '#a5b8ff'}`;
+    document.getElementById("cardTempo").style.background = `${kpis[4] != "00:00:00" ? '#ffc0c0' : '#a5b8ff'}`;
 
     if (graficoLinha != null) {
         graficoLinha.destroy();
